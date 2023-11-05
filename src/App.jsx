10091e187c976bbda377bf8d72d27fa158a578ca
Szymon Gąsiewski <szymongasiewski999@gmail.com>
2023-11-05 19:16:43 +0100

    prettier settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Register from './pages/Register'
-import Login from './pages/Login'
-import Error from './pages/Error'
-import SharedLayout from './pages/SharedLayout'
-import { AuthProvider } from './context/AuthContext'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Error from "./pages/Error";
+import SharedLayout from "./pages/SharedLayout";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
         <Routes>
-          <Route path='/' element={<SharedLayout />}>
+          <Route path="/" element={<SharedLayout />}>
             <Route index element={<Home />} />
-            <Route path='signup' element={<Register />} />
-            <Route path='signin' element={<Login />} />
-            <Route path='*' element={<Error />} />
+            <Route path="signup" element={<Register />} />
+            <Route path="signin" element={<Login />} />
+            <Route path="*" element={<Error />} />
           </Route>
         </Routes>
       </AuthProvider>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
