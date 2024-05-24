61324ecca84dcb5b3a7283a7ddd4ecdd276b9489
szymongasiewski <szymongasiewski999@gmail.com>
2024-05-25 01:15:40 +0200

    response image after detection is now clickable, checking input images sizes and types, block size max size changed, error messages updated

--- src/components/ChangeDetectionForm.jsx
+++ src/components/ChangeDetectionForm.jsx
@@ -35,9 +35,9 @@ const ChangeDetectionForm = ({
             className="block w-1/2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6"
             id="blocksize"
             name="blocksize"
-            placeholder="Provide block size (2-10)"
+            placeholder="Provide block size (2-5)"
             type="number"
-            max={10}
+            max={5}
             min={2}
             {...blockSizeAtribs}
           />
--- src/components/ResponseImage.jsx
+++ src/components/ResponseImage.jsx
@@ -3,7 +3,9 @@ import PropTypes from "prop-types";
 const ResponseImage = ({ imageUrl }) => {
   return (
     <div className="bg-gray-200 m-6 p-6 border-dashed border-2 rounded-md border-gray-500">
-      <img className="max-w-full" src={imageUrl} alt="response" />
+      <a href={imageUrl}>
+        <img className="max-w-full" src={imageUrl} alt="response" />
+      </a>
     </div>
   );
 };
